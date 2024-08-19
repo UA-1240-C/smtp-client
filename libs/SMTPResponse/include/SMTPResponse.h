36e431777b4d829f0f559bac2a445aa108e5e8a4
Zikkend <denzik04@gmail.com>
2024-08-19 17:45:42 +0300

    replaces u_int16_t with uint16_t  and added #include <cstdint>

--- libs/SMTPResponse/include/SMTPResponse.h
+++ libs/SMTPResponse/include/SMTPResponse.h
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <regex>
+#include <cstdint>
 
 #include "StatusType.h"
 
@@ -11,12 +12,12 @@ class SMTPResponse {
 public:
     explicit SMTPResponse(const std::string& response);
 
-    u_int16_t get_code() const;
+    uint16_t get_code() const;
     std::string get_enhanced_code() const;
     std::string get_text() const;
 
     StatusType get_status() const;
-    bool CodeEquals(u_int16_t code) const;
+    bool CodeEquals(uint16_t code) const;
     bool StatusEquals(StatusType status) const;
     std::string get_formated_response() const;
     std::string get_raw_response() const;
@@ -29,7 +30,7 @@ private:
     bool IsValidResponse(const std::string& response, std::smatch& matches) const;
     auto SplitAtEndline(const std::string& response) const -> std::pair<std::string, std::string>;
 
-    u_int16_t m_code;
+    uint16_t m_code;
     std::string m_enhanced_code;
     std::string m_text;
     std::string m_formated_response;
