dbe727e166c6092ea6e012a273aa324eac3072cd
Zikkend <denzik04@gmail.com>
2024-10-01 17:20:27 +0300

    feat: add clearing of log file

--- libs/Benchmark/include/Listener.h
+++ libs/Benchmark/include/Listener.h
@@ -11,7 +11,7 @@ namespace ISXBenchmark {
 
 class Listener {
 public:
-    Listener() = default;
+    Listener(const std::string& filename = "results.log");
 
     void LogResult(int thread_id, double time_taken, TimerResults& results, bool success);
 
@@ -20,6 +20,7 @@ public:
 private:
     std::mutex m_mutex; 
     std::vector<SamplerResults> m_results;
+    const std::string LOG_FILE_NAME;
 };
 
 }
--- libs/Benchmark/src/Listener.cpp
+++ libs/Benchmark/src/Listener.cpp
@@ -2,11 +2,15 @@
 
 namespace ISXBenchmark {
 
+Listener::Listener(const std::string& filename) : LOG_FILE_NAME("../logs/" + filename)
+{
+    std::ofstream log_file(LOG_FILE_NAME, std::ios::trunc);
+}
 
 void Listener::LogResult(int thread_id, double time_taken, TimerResults& results, bool success) {
     std::lock_guard<std::mutex> lock(m_mutex);
     m_results.emplace_back(time_taken, results, success);
-    std::ofstream log_file("../logs/results.log", std::ios::app);
+    std::ofstream log_file(LOG_FILE_NAME, std::ios::app);
     log_file << "Thread " << thread_id << ": Time = " << time_taken << "s, " << results <<" Success = " << success << "\n";
     log_file.close();
 }
