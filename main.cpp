8b90a7002b2233eb046e8e8c3885b24b98c935e1
Zikkend <denzik04@gmail.com>
2024-10-11 13:12:19 +0300

    hotfix

--- main.cpp
+++ main.cpp
@@ -5,6 +5,11 @@ using namespace ISXBenchmark;
 
 int main()
 {
-    ThreadManager tm(20, std::chrono::duration<double>(1), 1, std::make_unique<SMTPSampler>());
-    std::cout << tm.Start(1) << std::endl;
+    uint16_t thread_number = 20;
+    std::chrono::duration<double> ramp_up_period(1);
+    uint16_t loop_count = 1;
+    ThreadManager tm(thread_number, ramp_up_period, loop_count, std::make_unique<SMTPSampler>());
+
+    uint16_t client_timeout = 2;
+    std::cout << tm.Start(client_timeout) << std::endl;
 }
